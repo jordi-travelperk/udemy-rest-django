d2a2a68e6ea5113ad8c82c49f34fd9650d2bd2d8
Jordi <jordi.lopez@travelperk.com>
2020-02-07 11:45:37 +0100

    Add create ingredient feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -28,7 +28,9 @@ class TagViewSet(viewsets.GenericViewSet,
 
 
 class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin):
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
+    """Manage ingredients in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
@@ -39,3 +41,7 @@ class IngredientViewSet(viewsets.GenericViewSet,
         return self.queryset.filter(
             user=self.request.user
         ).order_by('-name').distinct()
+
+    def perform_create(self, serializer):
+        """Create a new tag"""
+        serializer.save(user=self.request.user)
